2e3ba25aef5d633bc5b66594ee4a9dba74ea47ad
csparker <qkrckstn78@naver.com>
2025-11-21 15:42:36 +0900

    컴포넌트완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
+    </BrowserRouter>
+    );
 };
 
 export default App;
 
 // import React from "react"; : 이 부분은 리액트를 사용하기 위해 리액트 라이브러리를 불러오고 있습니다. 이렇게 하면 리액트의 기능과 컴포넌트를 사용할 수 있습니다.
-// const App = () => { ... }; : App이라는 이름의 상수(const)를 선언하고, 이는 화살표 함수(arrow function)로 정의된 함수형 컴포넌트입니다. 함수형 컴포넌트는 입력(props)을 받아서 화면에 렌더링할 JSX 요소를 반환하는 역할을 합니다.
-// return <div>App</div> : 함수형 컴포넌트에서 반환하는 JSX 요소입니다. JSX는 JavaScript XML의 약자로, 리액트에서 컴포넌트의 모양을 정의하는 데 사용됩니다. 이 경우 <div> 요소로 "App"이라는 텍스트를 감싸고 있습니다.
-// export default App; : App 컴포넌트를 다른 파일에서 재사용할 수 있도록 내보내기(export) 설정을 하고 있습니다.
-// 
+// import { BrowserRouter, Route, Routes } from "react-router-dom"; : 이 부분은 react-router-dom에서 필요한 모듈들을 불러오고 있습니다. BrowserRouter, Route, Routes는 라우팅을 구현하는 데 필요한 컴포넌트들입니다.
+// import HomeView from "./views/HomeView"; : HomeView라는 이름으로, "./views/HomeView" 경로에 있는 컴포넌트를 불러오고 있습니다. 이는 HomeView 컴포넌트가 해당 경로에 있는 파일에서 정의되어 있다는 것을 의미합니다.
+//  const App = () => { ... }; : 이는 화살표 함수(arrow function)로 정의된 함수형 컴포넌트입니다. 이 함수형 컴포넌트는 리액트 애플리케이션의 최상위 컴포넌트로, 라우팅 설정을 포함하고 있습니다.
+// <BrowserRouter> : BrowserRouter는 라우팅을 적용하기 위한 컴포넌트입니다. 애플리케이션의 최상위 컴포넌트인 App 컴포넌트를 감싸서 라우팅 설정을 적용할 수 있도록 합니다.
+// <Routes> : Routes는 라우트들을 정의하는 컨테이너입니다. 각각의 <Route> 컴포넌트들을 <Routes> 컴포넌트 내부에 정의하여 경로와 컴포넌트를 매핑합니다.
+// <Route path="/" element={<HomeView />} /> : 이 부분은 "/" 경로에 해당하는 <HomeView> 컴포넌트를 렌더링하도록 설정하는 것입니다. path 속성에는 경로를 지정하고, element 속성에는 해당 경로에 렌더링할 컴포넌트를 JSX 문법으로 전달합니다.
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact=()=>{
+    return <div>Contact</div>
+}
+
+export default Contact;
+
+// //import React from "react"; : 이 부분은 리액트를 사용하기 위해 리액트 라이브러리를 불러오고 있습니다. 이렇게 하면 리액트의 기능과 컴포넌트를 사용할 수 있습니다.
+// const Contact = () => { ... }; : Contact라는 이름의 상수(const)를 선언하고, 이는 화살표 함수(arrow function)로 정의된 함수형 컴포넌트입니다. Contact 컴포넌트는 간단한 화면 요소를 렌더링하는 데 사용됩니다.
+// return <div>Contact</div>; : 함수의 본문(body)에 JSX 문법을 사용하여 UI를 정의하고 있습니다. JSX 문법은 리액트에서 UI를 구성하는 간편한 방법으로, HTML과 유사한 형태로 작성할 수 있습니다. 이 경우 <div> 태그로 감싸진 "Contact" 텍스트를 반환하여 화면에 렌더링합니다. 
+// export default Contact; : Contact 컴포넌트를 외부에서 재사용할 수 있도록 내보내기(export) 설정을 하고 있습니다. 이렇게 다른 파일에서 해당 컴포넌트를 불러와 사용할 수 있습니다
